00b6d84164a9b97774d5d45aec7d2a4ec0c1a413
Kirthika Vijay <1000055671@hexaware.com>
2022-10-12 21:47:29 +0530

    models added

---
+++ CRMS/Models/Bookings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMS.Models
+{
+    public class Bookings
+    {
+        [Key]
+        [Column(TypeName = "Guid")]
+        public Guid BookingId { get; set; }
+
+        [Column(TypeName = "Guid")]
+        public Guid ConfRoomID { get; set; }
+
+        [Column(TypeName = "Guid")]
+        public Guid BookedByUserId { get; set; }
+
+        [Column(TypeName = "Guid")]
+        public Guid BookedForDeptID { get; set; }
+
+        [Column(TypeName = "int")]
+        public int BookedForCapacity { get; set; }
+
+        [Column(TypeName = "nvarchar(40)")]
+        public string BookingStartDate { get; set; }
+
+        [Column(TypeName = "nvarchar(40)")]
+        public string BookingStartTime { get; set; }
+
+        [Column(TypeName = "nvarchar(40)")]
+        public string BookingEndDate { get; set; }
+
+        [Column(TypeName = "nvarchar(40)")]
+        public string BookingEndTime { get; set; }
+
+        [Column(TypeName = "bool")]
+        public bool BookingStatus { get; set; }
+
+
+    }
+}
---
+++ CRMS/Models/ConferenceRooms.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMS.Models
+{
+    public class ConferenceRooms
+    {
+        [Key]
+        [Column(TypeName = "Guid")]
+        public int ConfRoomId { get; set; }
+
+        [Column(TypeName = "Guid")]
+        public int ConfRoomTypeId { get; set; }
+
+        [Column(TypeName = "int")]
+        public int ConfRoomCapacity { get; set; }
+
+        [Column(TypeName = "Guid")]
+        public int ConfRoomManagerId { get; set; }
+
+        [Column(TypeName = "nvarchar(1000)")]
+        public string ConfRoomLocation { get; set; }
+
+        ConferenceRoomType conferenceRoomType { get; set; }
+
+        Users users { get; set; }
+
+    }
+}
